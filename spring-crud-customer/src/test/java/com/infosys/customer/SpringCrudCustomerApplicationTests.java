33d6eb5b7ed623efd02ad43e98ee4be78661512a
Sajeed <msajeed00@gmail.com>
2025-04-23 20:20:31 +0530

    used h2 database & profile for test

--- spring-crud-customer/src/test/java/com/infosys/customer/SpringCrudCustomerApplicationTests.java
+++ spring-crud-customer/src/test/java/com/infosys/customer/SpringCrudCustomerApplicationTests.java
@@ -3,11 +3,11 @@ package com.infosys.customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringCrudCustomerApplicationTests {
+@SpringBootTest(properties = "spring.profiles.active=test")  // Activate 'test' profile
+public class SpringCrudCustomerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This will load the application context using the 'test' profile
 	}
-
 }
